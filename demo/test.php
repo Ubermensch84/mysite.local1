80c6e2fa8fea8c161d01f55673884d1436c65e8b
Kirill <llirik_votan@rambler.ru>
2015-08-01 23:43:58 +0300

    Add files

---
+++ demo/test.php
@@ -0,0 +1,22 @@
+<?php
+
+// setcookie("color", $color);
+// echo "Привет!";
+// $color = 'red';
+
+
+// $arr = ['name' => 'John', 'age' => 25, true];
+// echo serialize($arr);
+
+
+// $user = ['name' => 'John',
+// 'login' => 'root',
+// 'passsword' => '1234'];
+
+// $str = serialize($user)		 ;
+// setcookie("user", $str);
+
+
+
+// $user = unserialize($_COOKIE["user"]);
+// print_r($user);
